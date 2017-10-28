719675b426aaba0babeb8e2b2e745e405dd62874
Alex Hoang Hai Nguyen <hoanghai92@hotmail.com>
2017-10-28 23:42:08 +0200

    feat: repeating background

--- Assets/Scripts/RepeatingBackground.cs
+++ Assets/Scripts/RepeatingBackground.cs
@@ -12,8 +12,11 @@ public class RepeatingBackground : MonoBehaviour {
 		screenWidthInPoints = height * Camera.main.aspect;
 	}
 
-	void AddRoom(float farhtestRoomEndX)
-	{
+	void FixedUpdate() {
+		GenerateRoomIfRequired();
+	}
+
+	void AddRoom(float farhtestRoomEndX) {
     // Pick a random index of the room to generate
     int randomRoomIndex = Random.Range(0, availableRooms.Length);
 
@@ -21,16 +24,65 @@ public class RepeatingBackground : MonoBehaviour {
     GameObject room = (GameObject)Instantiate(availableRooms[randomRoomIndex]);
 
     //3
-    float roomWidth = room.transform.Find("Day").localScale.x;
+    float roomWidth = room.transform.Find("Ground").localScale.x;
+		
+    //4
+    float roomCenter = farhtestRoomEndX + roomWidth * 0.5f;
 
-    // //4
-    // float roomCenter = farhtestRoomEndX + roomWidth * 0.5f;
+  	//5
+    room.transform.position = new Vector3(roomCenter, 0, 0);
 		//
-    // //5
-    // room.transform.position = new Vector3(roomCenter, 0, 0);
-		//
-    // //6
-    // currentRooms.Add(room);
+    //6
+    currentRooms.Add(room);
 	}
 
+	void GenerateRoomIfRequired(){
+    //1
+    List<GameObject> roomsToRemove = new List<GameObject>();
+
+    //2
+    bool addRooms = true;
+
+    //3
+    float playerX = transform.position.x;
+
+    //4
+    float removeRoomX = playerX - screenWidthInPoints;
+
+    //5
+    float addRoomX = playerX + screenWidthInPoints;
+
+    //6
+    float farthestRoomEndX = 0;
+
+    foreach(var room in currentRooms)
+    {
+        //7
+				float roomWidth = room.transform.Find("Ground").localScale.x;
+        float roomStartX = room.transform.position.x - (roomWidth * 0.5f) -1;
+        float roomEndX = roomStartX + roomWidth;
+
+        //8
+        if (roomStartX > addRoomX)
+            addRooms = false;
+
+        //9
+        if (roomEndX < removeRoomX)
+            roomsToRemove.Add(room);
+
+        //10
+        farthestRoomEndX = Mathf.Max(farthestRoomEndX, roomEndX);
+    }
+
+    //11
+    foreach(var room in roomsToRemove)
+    {
+        currentRooms.Remove(room);
+        Destroy(room);
+    }
+
+    //12
+    if (addRooms)
+        AddRoom(farthestRoomEndX);
+		}
 }
