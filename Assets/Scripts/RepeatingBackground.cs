f753925edb445b4049d4fd7c69075968669cf4cc
Alex Hoang Hai Nguyen <hoanghai92@hotmail.com>
2017-11-07 12:37:19 +0100

    flip now works

--- Assets/Scripts/RepeatingBackground.cs
+++ Assets/Scripts/RepeatingBackground.cs
@@ -23,54 +23,50 @@ public class RepeatingBackground : MonoBehaviour {
     //Creates a room object form the array of available rooms under the random index aboves
     GameObject room = (GameObject)Instantiate(availableRooms[randomRoomIndex]);
 
-    //3
     float roomWidth = room.transform.Find("Ground").localScale.x;
 		
-    //4
     float roomCenter = farhtestRoomEndX + roomWidth * 0.5f;
 
-  	//5
     room.transform.position = new Vector3(roomCenter, 0, 0);
-		//
-    //6
-    currentRooms.Add(room);
+
+		currentRooms.Add(room);
 	}
 
-	void GenerateRoomIfRequired(){
-    //1
-    List<GameObject> roomsToRemove = new List<GameObject>();
 
-    //2
+	void GenerateRoomIfRequired(){
+
+	List<GameObject> roomsToRemove = new List<GameObject>();
+
     bool addRooms = true;
 
-    //3
+   
     float playerX = transform.position.x;
 
-    //4
+    
     float removeRoomX = playerX - screenWidthInPoints;
 
-    //5
+    
     float addRoomX = playerX + screenWidthInPoints;
 
-    //6
+    
     float farthestRoomEndX = 0;
 
     foreach(var room in currentRooms)
     {
-        //7
+        
 				float roomWidth = room.transform.Find("Ground").localScale.x;
         float roomStartX = room.transform.position.x - (roomWidth * 0.5f) -1;
         float roomEndX = roomStartX + roomWidth;
 
-        //8
+        
         if (roomStartX > addRoomX)
             addRooms = false;
 
-        //9
+        
         if (roomEndX < removeRoomX)
             roomsToRemove.Add(room);
 
-        //10
+        
         farthestRoomEndX = Mathf.Max(farthestRoomEndX, roomEndX);
     }
 
