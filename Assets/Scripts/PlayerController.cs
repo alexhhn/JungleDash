cf0ca4c254b01bcaf6fa08de6067077e35db136a
Alex Hoang Hai Nguyen <hoanghai92@hotmail.com>
2017-11-07 01:43:55 +0100

    add flip functions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,31 +8,35 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
 	private int count;				//Integer to store the number of pickups collected so far.
 
-	public Transform groundCheck;
-	public float groundCheckRadius;
-	public LayerMask whatIsground;
+	public LayerMask GetGround;
 	private bool onGround;
-
+	private Collider2D playerCollider;
+	private bool isFlipped;
+	private float yPosition;
 
 	// Use this for initialization
 	void Start()
 	{
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
+		playerCollider = GetComponent<Collider2D> ();
+		yPosition = 0;
 
 	}
 
 	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
 	void FixedUpdate()
 	{
-		// onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsground);
+	//		onGround = true;
+
+		onGround = playerCollider.IsTouchingLayers(GetGround);
 
 		// Player moves forward automatically
 		rb2d.velocity = new Vector2(GameController.instance.runSpeed, rb2d.velocity.y);
 
 	 	// Player is jumping
-		bool isJumping = Input.GetKey(KeyCode.Space);
-		if(isJumping) {
+		bool isJumping = Input.GetKeyDown(KeyCode.Space);
+		if(isJumping && onGround) {
 			rb2d.velocity = new Vector2(0, jumpPower);
 		}
 
@@ -40,5 +44,30 @@ public class PlayerController : MonoBehaviour {
 		// {
 		// 	rb2d.velocity = new Vector2 (rb2d.velocity.x, jumpPower);
 		// }
+		Flip();
 	}
+
+	void Flip() {
+		if(Input.GetKeyDown(KeyCode.S)) {
+			transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * -1,transform.localScale.z);
+			if(isFlipped) {
+				yPosition = 0;
+			}  else {
+				yPosition = 5.5f;
+				isFlipped = true;
+			}
+			transform.position = new Vector3(transform.position.x, yPosition,transform.position.z);
+
+			rb2d.gravityScale = rb2d.gravityScale * -1;
+		}
+
+		// if(Input.GetKeyDown(KeyCode.A)) {
+		// 	Vector3 lTemp = transform.localScale;
+		// 		lTemp.y *= -1;
+		// 		transform.localScale = lTemp;
+		// 	transform.position = new Vector3(transform.position.x, 5.5f,transform.position.z);
+		// 	rb2d.gravityScale = -1;
+		// }
+	}
+
 }
