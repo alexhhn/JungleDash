484bca22942a795cdb4e5c3182d172986ae17ac1
Alex Hoang Hai Nguyen <hoanghai92@hotmail.com>
2017-11-21 15:38:46 +0100

    add menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public Button startText;
+	public Button settingsText;
+
+	// Use this for initialization
+	void Start () {
+		startText = startText.GetComponent<Button>();
+		settingsText = settingsText.GetComponent<Button>();
+	}
+
+	public void Play() {
+		SceneManager.LoadScene (1);
+	}
+}
